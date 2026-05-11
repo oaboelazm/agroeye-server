9da775eadda81b7fdcdb7d152e1cbca3de60be97
Omar AboElAzm <o.aboelazm@outlook.com>
2026-05-12 02:08:54 +0300

    Hashing with Argon2

--- api/api_server.py
+++ api/api_server.py
@@ -80,7 +80,7 @@ if not SECRET_KEY:
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/mobile/auth/login")
 
 
